5f41b937f6e997a186005c1fc291c6b5174f3410
Mohammad.M <mohammad.m.zareyi@ut.ac.ir>
2019-02-12 14:53:48 +0330

    crop+examples

--- edit/upedit/urls.py
+++ edit/upedit/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     #path('edit/<int:key>/bl/', views.bl, name='bl'),
     re_path(r'^edit/(?P<key>[0-9]*)/bl', views.bl, name='bl'),
     path('edit/<int:key>/rt/', views.rotatation, name='rt'),
+    path('edit/<int:key>/rs/', views.rotatation, name='rs'),
+    path('edit/<int:key>/cr/', views.croper, name='cr'),
 ]
--- edit/upedit/views.py
+++ edit/upedit/views.py
@@ -31,6 +31,7 @@ def edit(request, key):
             'pic_url':'media/%s' % im.name,
             'rotate_url':'rt/',
             'resize_url':'rs/',
+            'crop_url':'cr/'
         }
         return render(request, 'edit.html', context)
 
@@ -53,5 +54,21 @@ def rotatation(request, key):
         im2edit = Image.open('media/%s' % im.name)
         im2edit = im2edit.rotate(
             int(request.POST['deg'])).save('media/%s' % im.name)
+        redirect_path = '/up/edit/%s/' % key
+        return redirect(redirect_path)
+
+
+def croper (request, key):
+    im = pic.objects.get(pk=key)
+    if im.edited:
+        return render(request, 'cannotedit.html')
+    else:
+        im2edit = Image.open('media/%s' % im.name)
+        x1 = int(request.POST['x1'])
+        y1 = int(request.POST['y1'])
+        x2 = int(request.POST['x2'])
+        y2 = int(request.POST['y2'])
+        im2edit = im2edit.crop((x1, y1, x2, y2))
+        im2edit.save('media/%s' % im.name)
         redirect_path = '/up/edit/%s/' % key
         return redirect(redirect_path)
